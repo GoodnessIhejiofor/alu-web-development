52fcbe6e3050200e519bf5d9b899019627021243
Oma_goodness <oluomaihejiofor@gmail.com>
2024-05-30 20:51:08 +0200

    correction

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -16,25 +16,23 @@ def view_all_users() -> str:
     return jsonify(all_users)
 
 
-def current_user(self, request=None):
-    """current user"""
-    header = self.authorization_header(request)
-    if header is None:
-        return "Unauthorized", 401
-    b64 = self.extract_base64_authorization_header(header)
-    if b64 is None:
-        return "Unauthorized", 401
-    decoded = self.decode_base64_authorization_header(b64)
-    if decoded is None:
-        return "Unauthorized", 401
-    user_info = self.extract_user_credentials(decoded)
-    if user_info is None:
-        return "Unauthorized", 401
-    email, pwd = user_info
-    user = self.user_object_from_credentials(email, pwd)
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def view_one_user(user_id: str = None) -> str:
+    """ GET /api/v1/users/:id
+    Path parameter:
+      - User ID
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
+    if user_id is None:
+        abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+    user = User.get(user_id)
     if user is None:
-        return "Unauthorized", 401
-    return "OK", 200
+        abort(404)
+    return jsonify(user.to_json())
 
 
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
